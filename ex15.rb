6fcbf8d6f9669c02eb975cc330c5f6b37f60f8c1
ahhchewy <olivia.louie@gmail.com>
2014-10-13 19:50:09 -0700

    Adds close() on txt and txt_again in script

--- ex15.rb
+++ ex15.rb
@@ -4,6 +4,7 @@ txt = open(filename)
 
 puts "Here's your file #{filename}:"
 puts txt.read
+txt.close
 puts
 
 print "Type the filename again:"
@@ -12,3 +13,4 @@ file_again = $stdin.gets.chomp
 txt_again = open(file_again)
 
 puts txt_again.read
+txt_again.close
