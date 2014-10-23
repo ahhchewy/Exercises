cc716553d7f07bcd0e3ae8b0ac894ed6cdb32b65
ahhchewy <olivia.louie@gmail.com>
2014-10-23 02:46:11 -0700

    Game skeleton, girl's bedroom not worked on

--- ex43.rb
+++ ex43.rb
@@ -1,6 +1,5 @@
 class Game
 
-
 	def initialize(room_map)
 
 		@quips = [
@@ -18,7 +17,6 @@ class Game
 
 		@room_map = room_map
 		@previous_room = @room_map
-		@orientation = nil
 	end
 
 	def play()
@@ -33,10 +31,6 @@ class Game
 
 	def prompt
 		print "> "
-	end
-
-	def compare(input)
-		@direction.any? {|x| x == input}
 	end
 
 	def death()
@@ -62,7 +56,6 @@ class Game
 		while user_input != 'yes' || user_input != 'no'
 
 			if user_input == 'yes'
-				puts "Carefully, you make your way to the entrance of the mouse hole."
 				return :mouse_hole
 			elsif user_input == 'no'
 				return :death
@@ -97,7 +90,7 @@ class Game
 					 else
 					 	puts "Was not able to proceed to the next room."
 					 end
-					 
+
 				elsif @previous_room == :hallway_1
 
 					@previous_room = :mouse_hole
@@ -126,130 +119,563 @@ class Game
 	end
 
 	def hallway_1()
-		puts @previous_room
+		puts "Previous room: #{@previous_room}"
 		puts
-		puts "Hallway1"
+		puts "You are in Hallway1"
+		prompt
+
+		user_input = $stdin.gets.chomp.downcase
+
+		while user_input != 'right' || user_input != 'left' || user_input != 'straight' || user_input != 'turn around'
+
+			if user_input == 'right' || user_input == 'left' || user_input == 'straight' || user_input == 'turn around'
+
+				if @previous_room == :mouse_hole
+
+					@previous_room = :hallway_1
+
+					if user_input == 'right'
+						return :hallway_2
+					elsif user_input == 'left'
+						return :stairs
+					elsif user_input == 'straight'
+						puts @wall
+						@previous_room = :mouse_hole
+					elsif user_input == 'turn around'
+						return :mouse_hole
+					else
+						puts "Was not able to proceed to the next room."
+					end
+
+				elsif @previous_room == :stairs
+
+					@previous_room = :hallway_1
+
+					if user_input == 'right'
+						return :mouse_hole
+					elsif user_input == 'left'
+						puts @wall
+						@previous_room = :stairs
+					elsif user_input == 'straight'
+						return :hallway_2
+					elsif user_input == 'turn around'
+						return :stairs
+					else
+						puts "Was not able to proceed to the next room."
+					end
+
+				elsif @previous_room == :hallway_2
+
+					@previous_room = :hallway_1
+
+					if user_input == 'right'
+						puts @wall
+						@previous_room = :hallway_2
+					elsif user_input == 'left'
+						return :mouse_hole
+					elsif user_input == 'straight'
+						return :hallway_1
+					elsif user_input == 'turn around'
+						return :stairs
+					else
+						puts "Was not able to proceed to the next room."
+					end
+				end
+			else
+				puts @misdirection
+			end
+
+			prompt
+			user_input = $stdin.gets.chomp.downcase
+		end
 	end
 
+	def hallway_2()
+		puts "Previous room: #{@previous_room}"
+		puts
+		puts "You are in Hallway2"
+		prompt
 
+		user_input = $stdin.gets.chomp.downcase
 
-class Hallway2
+		while user_input != 'right' || user_input != 'left' || user_input != 'straight' || user_input != 'turn around'
 
-	def enter()
-		puts "Hallway2"
+			if user_input == 'right' || user_input == 'left' || user_input == 'straight' || user_input == 'turn around'
+
+				if @previous_room == :hallway_1
+
+					@previous_room = :hallway_2
+
+					if user_input == 'right'
+						return :livingroom
+					elsif user_input == 'left'
+						return :kitchen
+					elsif user_input == 'straight'
+						return :dead_end
+					elsif user_input == 'turn around'
+						return :hallway_1
+					else
+						puts "Was not able to proceed to the next room."
+					end
+
+				elsif @previous_room == :livingroom
+
+					@previous_room = :hallway_2
+
+					if user_input == 'right'
+						return :dead_end
+					elsif user_input == 'left'
+						return :hallway_1
+					elsif user_input == 'straight'
+						return :kitchen
+					elsif user_input == 'turn around'
+						return :livingroom
+					else
+						puts "Was not able to proceed to the next room."
+					end
+
+				elsif @previous_room == :dead_end
+
+					@previous_room = :hallway_2
+
+					if user_input == 'right'
+						return :kitchen
+					elsif user_input == 'left'
+						return :livingroom
+					elsif user_input == 'straight'
+						return :hallway_1
+					elsif user_input == 'turn around'
+						return :dead_end
+					else
+						puts "Was not able to proceed to the next room."
+					end
+				end
+			else
+				puts @misdirection
+			end
+
+			prompt
+			user_input = $stdin.gets.chomp.downcase
+		end
 	end
-end
 
+	def livingroom()
+		puts "Previous room: #{@previous_room}"
+		puts
+		puts "You are in living room"
+		puts "What will you do?"
+		prompt
 
-class Livingroom
+		user_input = $stdin.gets.chomp.downcase
 
-	def enter()
-		puts "Hallway3"
+		while user_input != 'turn around'
+
+			if user_input == 'turn around'
+
+				@previous_room = :livingroom
+
+				return :hallway_2
+			elsif user_input == 'explore'
+				return :death #eaten by cat
+			else
+				puts "Unsure what to do, you stare blankly into the livingroom."
+			end
+
+			prompt
+			user_input = $stdin.gets.chomp.downcase
+		end
 	end
-end
 
+	def dead_end()
+		puts "Previous room: #{@previous_room}"
+		puts
+		puts "You are in DeadEnd"
+		prompt
 
-class DeadEnd
+		user_input = $stdin.gets.chomp.downcase
 
-	def enter()
-		puts "DeadEnd"
+		while user_input != 'right' || user_input != 'left' || user_input != 'straight' || user_input != 'turn around'
+
+			if user_input == 'right' || user_input == 'left' || user_input == 'straight' || user_input == 'turn around'
+
+				@previous_room = :dead_end
+
+				if user_input == 'right'
+					puts @wall
+				elsif user_input == 'left'
+					puts @wall
+				elsif user_input == 'straight'
+					return :door
+				elsif user_input == 'turn around'
+					return :hallway_2
+				else
+					puts "Was not able to proceed to the next room."
+				end
+
+			else
+				puts @misdirection
+			end
+
+			prompt
+			user_input = $stdin.gets.chomp.downcase
+		end
 	end
-end
 
+	def door()
+		puts "Previous room: #{@previous_room}"
+		puts
+		puts "You are in Door"
+		puts "What will you do?"
+		prompt
 
-class Door
-	def enter()
-		puts "Door"
+		user_input = $stdin.gets.chomp.downcase
+
+		while user_input != 'turn around'
+
+			if user_input == 'turn around'
+
+				@previous_room = :dead_end
+
+				return :hallway_2
+			elsif user_input != 'turn around'
+				return :death #door smoosh
+			else
+				puts "Unsure what to do, you stare blankly at the door."
+			end
+
+			prompt
+			user_input = $stdin.gets.chomp.downcase
+		end
 	end
-end
 
+	def stairs()
+		puts "Previous room: #{@previous_room}"
+		puts
+		puts "You are in Stairs"
+		puts "Do you go up the stairs?"
+		prompt
 
-class Kitchen
+		user_input = $stdin.gets.chomp.downcase
 
-	def enter()
-		puts "Kitchen"
+		while user_input != 'yes' || user_input != 'no'
+
+			if user_input == 'yes'
+
+				@previous_room = :stairs
+
+				return :top_stairs
+			elsif user_input == 'no'
+				return :hallway_1
+			else
+				puts "Unsure what to do, you stare blankly up at the stairs."
+			end
+
+			prompt
+			user_input = $stdin.gets.chomp.downcase
+		end		
 	end
-end
 
+	def top_stairs()
+		puts "Previous room: #{@previous_room}"
+		puts
+		puts "You are in TopStairs"
+		prompt
 
-class Stairs
+		user_input = $stdin.gets.chomp.downcase
 
-	def enter()
-		puts "Stairs"
+		while user_input != 'right' || user_input != 'left' || user_input != 'straight' || user_input != 'turn around'
+
+			if user_input == 'right' || user_input == 'left' || user_input == 'straight' || user_input == 'turn around'
+
+				if @previous_room == :stairs
+
+					@previous_room = :top_stairs
+
+					if user_input == 'right'
+						return :hallway_3
+					elsif user_input == 'left'
+						puts @wall
+						@previous_room = stairs
+					elsif user_input == 'straight'
+						return :girls_bedroom
+					elsif user_input == 'turn around'
+						return :stairs
+					else
+						puts "Was not able to proceed to the next room."
+					end
+
+				elsif @previous_room == :girls_bedroom
+
+					@previous_room = :top_stairs
+
+					if user_input == 'right'
+						puts @wall
+						@previous_room = :girls_bedroom
+					elsif user_input == 'left'
+						return :hallway_3
+					elsif user_input == 'straight'
+						return :stairs
+					elsif user_input == 'turn around'
+						return :girls_bedroom
+					else
+						puts "Was not able to proceed to the next room."
+					end
+
+				elsif @previous_room == :hallway_3
+
+					@previous_room = :top_stairs
+
+					if user_input == 'right'
+						return :girls_bedroom
+					elsif user_input == 'left'
+						return :stairs
+					elsif user_input == 'straight'
+						puts @wall
+						@previous_room = :hallway_3
+					elsif user_input == 'turn around'
+						return :hallway_3
+					else
+						puts "Was not able to proceed to the next room."
+					end
+				end
+			else
+				puts @misdirection
+			end
+
+			prompt
+			user_input = $stdin.gets.chomp.downcase
+		end
 	end
-end
 
+	def hallway_3()
+		puts "Previous room: #{@previous_room}"
+		puts
+		puts "You are in Hallway3"
+		prompt
 
-class TopStairs
+		user_input = $stdin.gets.chomp.downcase
 
-	def enter()
-		puts "TopStairs"
+		while user_input != 'right' || user_input != 'left' || user_input != 'straight' || user_input != 'turn around'
+
+			if user_input == 'right' || user_input == 'left' || user_input == 'straight' || user_input == 'turn around'
+
+				if @previous_room == :stairs
+
+					@previous_room = :hallway_3
+
+					if user_input == 'right'
+						@previous_room = :stairs
+						return :banister
+					elsif user_input == 'left'
+						return :bathroom
+					elsif user_input == 'straight'
+						return :master_bedroom
+					elsif user_input == 'turn around'
+						return :stairs
+					else
+						puts "Was not able to proceed to the next room."
+					end
+
+				elsif @previous_room == :bathroom
+
+					@previous_room = :hallway_3
+
+					if user_input == 'right'
+						return :top_stairs
+					elsif user_input == 'left'
+						return :master_bedroom
+					elsif user_input == 'straight'
+						@previous_room = :bathroom
+						return :banister
+					elsif user_input == 'turn around'
+						return :bathroom
+					else
+						puts "Was not able to proceed to the next room."
+					end
+
+				elsif @previous_room == :master_bedroom
+
+					@previous_room = :hallway_3
+
+					if user_input == 'right'
+						return :bathroom
+					elsif user_input == 'left'
+						@previous_room = :master_bedroom
+						return :banister
+					elsif user_input == 'straight'
+						return :hallway_3
+					elsif user_input == 'turn around'
+						return :master_bedroom
+					else
+						puts "Was not able to proceed to the next room."
+					end
+				end
+			else
+				puts @misdirection
+			end
+
+			prompt
+			user_input = $stdin.gets.chomp.downcase
+		end
 	end
-end
 
-
-class Hallway3
-
-	def enter()
-		puts "Hallway3"
+	def girls_bedroom()
+		puts "Previous room: #{@previous_room}"
+		puts
+		puts "You are in GirlBedroom"
 	end
-end
 
+	def banister()
+		puts "Previous room: #{@previous_room}"
+		puts
+		puts "You are in Banister"
+		puts "Careful!"
+		puts "You teeter over the edge!"
 
-class GirlBedroom
+		chances_of_falling = rand(5)
 
-	def enter()
-		puts "GirlBedroom"
+		if chances_of_falling == 0
+			puts "You've fallen over the edge!"
+			return :death
+		else
+			puts "Phew! Close call!"
+			puts "You scurry away from the banister."
+			return hallway_3
+		end
 	end
-end
 
+	def bathroom()
+		puts "Previous room: #{@previous_room}"
+		puts
+		puts "You are in Bathroom"
+		puts "What will you do?"
+		prompt
 
-class Banister
-	def enter()
-		puts "Banister"
+		user_input = $stdin.gets.chomp.downcase
+
+		while user_input != 'turn around'
+
+			if user_input == 'turn around'
+
+				@previous_room = :bathroom
+
+				return :hallway_3
+			elsif user_input == 'explore'
+				return :death #drown in toilet
+			else
+				puts "Unsure what to do, you stare blankly into the bathroom."
+			end
+
+			prompt
+			user_input = $stdin.gets.chomp.downcase
+		end
 	end
-end
 
+	def master_bedroom()
+		puts "Previous room: #{@previous_room}"
+		puts
+		puts "You are in MasterBedroom"
+		puts "What will you do?"
+		prompt
 
-class Bathroom
+		user_input = $stdin.gets.chomp.downcase
 
-	def enter()
+		while user_input != 'turn around'
 
+			if user_input == 'turn around'
+
+				@previous_room = :master_bedroom
+
+				return :hallway_3
+			elsif user_input == 'explore'
+				return :death #Discovered! Impaled by stiletto
+			else
+				puts "Unsure what to do, you stare blankly into the master bedroom."
+			end
+
+			prompt
+			user_input = $stdin.gets.chomp.downcase
+		end
 	end
-end
 
+	def kitchen()
+		puts "Previous room: #{@previous_room}"
+		puts
+		puts "You are in Kitchen"
+		puts "What will you do?"
+		prompt
 
-class MasterBedroom
+		user_input = $stdin.gets.chomp.downcase
 
-	def enter()
-		puts "MasterBedroom"
+		while user_input != 'turn around'
+
+			if user_input == 'turn around'
+
+				@previous_room = :kitchen
+
+				return :hallway_2
+			elsif user_input == 'explore'
+				return :inner_kitchen
+			else
+				puts "Unsure what to do, you stare blankly into the kitchen."
+			end
+
+			prompt
+			user_input = $stdin.gets.chomp.downcase
+		end
 	end
-end
 
+	def inner_kitchen()
+		puts "Previous room: #{@previous_room}"
+		puts
+		puts "You are in InnerKitchen"
+		puts "What will you do?"
+		prompt
 
-class Kitchen
+		user_input = $stdin.gets.chomp.downcase
 
-	def enter()
-		puts "Kitchen"
+		while user_input != 'turn around'
+
+			if user_input == 'turn around'
+				return :hallway_2
+			elsif user_input == 'explore'
+				return :final
+			else
+				puts "Unsure what to do, you stare blankly into the kitchen."
+			end
+
+			prompt
+			user_input = $stdin.gets.chomp.downcase
+		end
 	end
-end
 
+	def final()
+		puts "Previous room: #{@previous_room}"
+		puts
+		puts "You encounter floor and counter"
+		puts "What will you do?"
+		prompt
 
-class InnerKitchen
+		user_input = $stdin.gets.chomp.downcase
 
-	def enter()
-		puts "InnerKitchen"
+		while user_input != 'turn around'
+
+			if user_input == 'turn around'
+				return :kitchen
+			elsif user_input.include?('climb')
+				puts "You found the cheese! You won!"
+				puts "You will not starve!"
+				exit(0)
+			elsif user_input == 'explore'
+				return :death # mouse trap
+			else
+				puts "Unsure what to do, you stare blankly into the kitchen."
+			end
+
+			prompt
+			user_input = $stdin.gets.chomp.downcase
+		end
 	end
-end
-
-
-class Finished
-
-	def enter()
-		puts "You found the cheese! You won!"
-		puts "You will not starve!"
-	end
-end
 
 end
 
