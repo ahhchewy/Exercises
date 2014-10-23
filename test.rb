cca0da4b0f55389ac03107995c5ca740c921481a
ahhchewy <olivia.louie@gmail.com>
2014-10-23 02:45:20 -0700

    Just dumping stuff here

--- test.rb
+++ test.rb
@@ -1,25 +1,6 @@
-misdirection = {
-	:wall => "You've walked into a wall.",
-	:confused => "Not knowing a direction to go in, you run around in a circle.",
-}
-
-def comparison(input)
-	direction = ['right', 'left', 'straight', 'turn around']
-	direction.any? {|x| x == input}
-end
-
-puts "At the entrance of the mouse hole."
-print "> "
-
-user_input = $stdin.gets.chomp.downcase
-
-while !comparison(user_input)
-	if comparison(user_input)
-	puts "This works"
+x = 9
+if x == 6 || 7 || 8
+	puts "works"
 else
-	puts "Nope, dosen't work"
-end
-	puts misdirection[:confused]
-	print "> "
-	user_input = $stdin.gets.chomp.downcase
+	puts "doesn't work"
 end
