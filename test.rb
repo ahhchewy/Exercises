07b32249c56b8c64cb8d52a06f4042f12be0ee3f
ahhchewy <olivia.louie@gmail.com>
2014-10-24 01:28:13 -0700

    testing death scenes

--- test.rb
+++ test.rb
@@ -1,6 +1,17 @@
-x = 9
-if x == 6 || 7 || 8
-	puts "works"
-else
-	puts "doesn't work"
-end
+		@death_scenes = {
+			:starvation => "You have give up on the search for food and starve!",
+			:smooshed => "The door suddenly opens and smooshes you against the wall!",
+			:cat => "A cat in the livingroom hunts you down and eats you!",
+			:suicide => "You fall over the edge and accidentally commit suicide!",
+			:toilet => "Mesmerized by the shiny giant bowl, you climb it to check it out. 
+				\nSYou slip and fall into the toilet and drown!",
+			:hamster => ,
+			:stiletto => "You are discovered by the lady of the house!
+				\nShe impales you with her stiletto!",
+			:trap => "You sniff around on the ground and catch the whiff of a bit of cheese.
+				\nFollowing the scent, you find the tiny morsel sitting on some weird contraption.
+				\n**SMACK**
+				\NYou unwittingly stumble onto a mouse trap and it has crushed your skull!",
+		}
+
+		puts :toilet
